3b81371f3462b91035a7edbae90c54e738ba258f
max <max@gabrielsson.com>
2020-07-17 10:14:01 +0200

    fixed tsx

--- init.el
+++ init.el
@@ -358,8 +358,47 @@
 (use-package tide
   :ensure t
   :after (typescript-mode company flycheck)
-  :hook ((typescript-mode . tide-hl-identifier-mode)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
          (before-save . tide-format-before-save)))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+;; NVM has latest node binary, make sure emacs finds it.
+;; Not optimal but works for now
+(setq exec-path (append exec-path '("~/.nvm/versions/node/v13.1.0/bin/")))
+
+(use-package prettier-js
+  :ensure t
+  :hook (
+         (js2-mode . prettier-js-mode)
+         (typescript-mode . prettier-js-mode)
+         ))
 
 (use-package rjsx-mode
   :ensure t
